581cfbcdb092bb89d6a4463660f887cd132e8417
Yosef Fourentino <yoseffourentino30@gmail.com>
2024-01-16 21:45:04 +0700

    halaman itama

---
+++ app.js
@@ -0,0 +1,18 @@
+const foodsApi = "https://www.themealdb.com/api/json/v1/1/categories.php"
+fetch(foodsApi)
+    .then((result) => result.json())
+    .then((data) => data.categories)
+    .then((foods) => foods.forEach(food =>{
+        let foodCategory = food.strCategory
+        let foodCategoryBg = food.strCategoryThumb
+        let categoryContainer = document.querySelector('.food-category-container')
+        let div = document.createElement('div')
+        let h3 = document.createElement('h3')
+
+        categoryContainer.append(div)
+        div.append(h3)
+        h3.append(foodCategory)
+        div.style.backgroundImage = `url(${foodCategoryBg})`
+        div.classList.add('food-category')
+        div.classList.add('bg-opacity')
+    }))
